7802f958823f4ba0a40663202039aa202ee33fbc
Sanuzzi <occhiuzzi.santiago@gmail.com>
2014-12-22 02:09:15 -0300

    Fix a la tabla de terminales.

--- tickets/front/js/controllers/asistenciaCtrl.js
+++ tickets/front/js/controllers/asistenciaCtrl.js
@@ -7,6 +7,7 @@ angular.module('reservasApp').controller('asistenciaCtrl',function($scope, $stat
 	primerDiaSolicitado.setHours(0,0,0,0);
 	var cuantosDiasMasCargar = porDefecto.getCuantosDiasMas();
 	$scope.sesiones = [];
+	$scope.terminales = [];
     ayuda.actualizarExplicaciones();
     $scope.margen = ayuda.getMargen();
 
@@ -33,7 +34,24 @@ angular.module('reservasApp').controller('asistenciaCtrl',function($scope, $stat
 			console.log('Se produjo un error al obtener las sesiones en ' + primerDiaSolicitado + ' y en los ' + (cuantosDiasMasCargar - 1) + ' d\xEDas siguientes' );
 		});
 	};
+
+	var obtenerTerminales = function() {
+		
+		var comportamientoSiRequestExitoso = function(terminalesRecibidas) {
+			$scope.terminales = terminalesRecibidas;
+		};
+		
+		servidor.obtenerTerminales()
+		.success(function(terminalesRecibidas, status, headers, config) {
+			comportamientoSiRequestExitoso(terminalesRecibidas);
+			console.log("Se pudieron obtener las terminales");
+		})
+		.error(function(terminalesRecibidas, status, headers, config) {
+			console.log('Se produjo un error al obtener las terminales');
+		});
+	};
 	
+	obtenerTerminales();
 	obtenerSesiones();
 
 	$scope.cargarMasDias = function(){
@@ -44,7 +62,7 @@ angular.module('reservasApp').controller('asistenciaCtrl',function($scope, $stat
 	$scope.nombreDeUsuario = function(idDeUsuario){
 		servidor.obtenerUnUsuario(idDeUsuario)
 		.success(function(elUsuario, status, headers, config) {
-			return elUsuario.name; // el apellido viene en el nombre, o + ' ' + elUsuario.surname;
+			return elUsuario.name;
 			console.log('Obtenidos los datos del usuario con id ' + idDeUsuario + ' exitosamente');
 		})
 		.error(function(data, status, headers, config) {
@@ -54,15 +72,21 @@ angular.module('reservasApp').controller('asistenciaCtrl',function($scope, $stat
 	};
 
 	$scope.nombreDeTerminal = function(idDeTerminal){
-		servidor.obtenerUnaTerminal(idDeTerminal)
-		.success(function(terminal, status, headers, config) {
+		var terminal = $scope.terminales.filter(function(unaTerminal){return unaTerminal.id == idDeTerminal})[0];
+
+		if(terminal){
 			return terminal.name;
-			console.log('Obtenidos los datos de la terminal con id ' + idDeTerminal + ' exitosamente');
-		})
-		.error(function(data, status, headers, config) {
-			return '(no disponible)';
-			console.log('Se produjo un error al obtener los datos de la terminal con id ' + idDeTerminal);
-		});
+		} else {
+			servidor.obtenerUnaTerminal(idDeTerminal)
+			.success(function(terminal, status, headers, config) {
+				console.log('Obtenidos los datos de la terminal con id ' + idDeTerminal + ' exitosamente');
+				return terminal.name;
+			})
+			.error(function(data, status, headers, config) {
+				console.log('Se produjo un error al obtener los datos de la terminal con id ' + idDeTerminal);
+				return '(no disponible)';
+			});
+		}
 	}
 
 	$scope.getOperacion = function(id){
